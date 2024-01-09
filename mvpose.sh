ce92ebaceb8e313db3deb79f6d8333684cc49779
Daniel <daniel@mail.de>
2024-01-09 13:35:17 +0100

    Print times.

--- mvpose.sh
+++ mvpose.sh
@@ -20,9 +20,10 @@ fi
 N_CAMS=$(ls -l $VID_DIR | grep -c ^d)
 
 echo "#cameras: $N_CAMS"
+echo "#$(date)"
 
-if [ ! -d "$POSE_DIR" ]; then
-    mkdir $POSE_DIR
+# if [ ! -d "$POSE_DIR" ]; then
+    mkdir -p $POSE_DIR
     echo "execute 2D pose estimation..."
     for ((CID=0; CID<$N_CAMS; CID++))
     do
@@ -30,14 +31,15 @@ if [ ! -d "$POSE_DIR" ]; then
         CAMERA=$(printf "camera%02d" $CID)
         POSE2D_INPUT="$VID_DIR/$CAMERA"
         POSE2D_OUTPUT="$POSE_DIR/$CAMERA"
-        mkdir $POSE2D_OUTPUT
+        mkdir -p $POSE2D_OUTPUT
         cd $OPENPOSE_DIR && ./openpose.sh $POSE2D_INPUT $POSE2D_OUTPUT
         cd $MAIN_DIR
     done
-else
-    echo "2D poses already estimated"
-fi
+# else
+#     echo "2D poses already estimated"
+# fi
 
+echo "#$(date)"
 
 DOCKER_VERSION=$(docker version --format '{{.Server.Version}}')
 echo "docker: $DOCKER_VERSION"
@@ -52,3 +54,5 @@ docker run\
     -v "$1":/home/user/dataset\
     jutanke/mv3dpose\
     /bin/bash exec.sh
+
+echo "#$(date)"
